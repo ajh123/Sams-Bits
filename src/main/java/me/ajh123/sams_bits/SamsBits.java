f1f75be56b5d801e3d459841f65f6fc08e1aafbe
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2024-11-03 13:05:52 +0000

    feat: add fabric_permissions_api

--- src/main/java/me/ajh123/sams_bits/SamsBits.java
+++ src/main/java/me/ajh123/sams_bits/SamsBits.java
@@ -1,9 +1,17 @@
 package me.ajh123.sams_bits;
 
+import me.ajh123.sams_bits.api.permissions.PermissionContainer;
+import me.lucko.fabric.api.permissions.v0.OfflinePermissionCheckEvent;
+import me.lucko.fabric.api.permissions.v0.PermissionCheckEvent;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.util.TriState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CompletableFuture;
 
 public class SamsBits implements ModInitializer {
 	public static final String MOD_ID = "sams_bits";
@@ -14,5 +22,21 @@ public class SamsBits implements ModInitializer {
 	public void onInitialize() {
 		LOGGER.info("Initializing content ...");
 		ModItems.initialize();
+
+//		PermissionCheckEvent.EVENT.register((source, permission) -> {
+//			if (isSuperAdmin(source)) {
+//				return TriState.TRUE;
+//			}
+//			return TriState.DEFAULT;
+//		});
+//
+//		OfflinePermissionCheckEvent.EVENT.register((uuid, permission) -> {
+//			return CompletableFuture.supplyAsync(() -> {
+//				if (isSuperAdmin(uuid)) {
+//					return TriState.TRUE;
+//				}
+//				return TriState.DEFAULT;
+//			});
+//		});
 	}
 }
