6c2174afe2e1c0e4353e3230f62357e868c515c9
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2024-12-30 17:47:36 +0000

    feat: implement NBT serialization for RoadNodeBlockEntity to handle destinations and sources

--- minecraft_common/src/main/java/me/ajh123/sams_bits/content/roads/RoadNodeBlockEntity.java
+++ minecraft_common/src/main/java/me/ajh123/sams_bits/content/roads/RoadNodeBlockEntity.java
@@ -7,6 +7,8 @@ import me.ajh123.sams_bits.content.registry.ModBlockEntities;
 import me.ajh123.sams_bits.roads.RoadNode;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.registry.RegistryWrapper.WrapperLookup;
 import net.minecraft.util.math.BlockPos;
 
 public class RoadNodeBlockEntity extends BlockEntity {
@@ -44,5 +46,24 @@ public class RoadNodeBlockEntity extends BlockEntity {
         return this.sources;
     }
 
+    @Override
+    protected void writeNbt(NbtCompound nbt, WrapperLookup registryLookup) {
+        super.writeNbt(nbt, registryLookup);
 
+        nbt.putLongArray("destinations", destinations);
+        nbt.putLongArray("sources", sources);
+    }
+
+    @Override
+    protected void readNbt(NbtCompound nbt, WrapperLookup registryLookup) {
+        super.readNbt(nbt, registryLookup);
+
+        for (long destination : nbt.getLongArray("destinations")) {
+            destinations.add(destination);
+        }
+
+        for (long source : nbt.getLongArray("sources")) {
+            sources.add(source);
+        }
+    }
 }
